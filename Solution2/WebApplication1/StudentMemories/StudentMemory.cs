65f8372dcd4c5a18f19b0a8a2a8192b8283483a8
NguyenBaKhanh170920 <146510766+NguyenBaKhanh170920@users.noreply.github.com>
2024-01-16 08:55:43 +0700

    Delete Solution2 directory

--- Solution2/WebApplication1/StudentMemories/StudentMemory.cs
+++
@@ -1,9 +0,0 @@
-﻿using WebApplication1.Applications.Entities;
-
-namespace WebApplication1.StudentMemories
-{
-    public class StudentMemory
-    {
-        public Dictionary<string, Student> Students { get; set; } = new Dictionary<string, Student>();
-    }
-}
