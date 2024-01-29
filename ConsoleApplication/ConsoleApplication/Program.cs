2a65490f5819fc6fe2d1080881e23301701b2585
NguyenBaKhanh170920 <146510766+NguyenBaKhanh170920@users.noreply.github.com>
2024-01-29 08:59:31 +0700

    Backend

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter first number:");
+int i1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number:");
+int i2 = int.Parse(Console.ReadLine());
+//Console.WriteLine("{0} and {1}", i1, i2);
+while (true)
+{
+    Console.WriteLine("Select operation(+,-,*,/):");
+    string op = Console.ReadLine();
+    switch (op)
+    {
+        case "+":
+            Console.WriteLine("Ket qua phep cong:{0}", i1 + i2); break;
+        case "-":
+            Console.WriteLine("Ket qua phep tru:{0}", i1 - i2); break;
+        case "*":
+            Console.WriteLine("Ket qua phep nhan:{0}", i1 * i2); break;
+        case "/":
+            if (i2 == 0)
+            {
+                Console.WriteLine("Ko the chia cho 0");
+            }
+            else
+            {
+                Console.WriteLine("Ket qua phep chia:{0}", i1 / i2);
+                Console.WriteLine("Phan du phep chia:{0}", i1 % i2);
+
+            }
+            break;
+        default:
+            Console.WriteLine("Unknown operaion");
+            break;
+    }
+}
+
+
+
---
+++ ConsoleApplication/ConsoleReadAndWriteFile/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace ConsoleReadAndWriteFile
+{
+    public class Program
+
+    {
+        public void WriteLogError(string message)
+        {
+            string logFolderName = "C:\\Users\\Administrator\\Documents\\PRN211\\New folder\\Training\\Backend\\ConsoleApplication\\ConsoleReadAndWriteFile";
+            if (!Directory.Exists(logFolderName))
+            {
+                Directory.CreateDirectory(logFolderName);
+            }
+            string logFileName = "";
+            DateTime now = DateTime.Now;
+            logFileName = String.Format("data.txt",
+                now.Year, now.Month, now.Day);
+            string fullFileLog = Path.Combine(logFolderName, logFileName);
+            using (StreamWriter sw = new StreamWriter(fullFileLog, true))
+            {
+                sw.WriteLine(String.Format("Thoi gian: {0}", now));
+                sw.WriteLine(String.Format("Tin nhan cu the: {0}", message));
+            }
+        }
+        public void ReadLog()
+        {
+            string logFolderName = "C:\\Users\\Administrator\\Documents\\PRN211\\New folder\\Training\\Backend\\ConsoleApplication\\ConsoleReadAndWriteFile";
+            if (Directory.Exists(logFolderName))
+            {
+
+            }
+            string logFileName = "";
+            DateTime now = DateTime.Now;
+            logFileName = String.Format("data.txt");
+            string fullFileLog = Path.Combine(logFolderName, logFileName);
+            string a = File.ReadAllText(fullFileLog);
+            Console.WriteLine(a);
+        }
+        public static void Main(string[] args)
+        {
+            Program cs = new Program();
+            while (true)
+            {
+                Console.WriteLine("Chon (Read/Write) data:");
+                string choose = Console.ReadLine();
+                if (String.Compare(choose, "Read", true) == 0)
+                {
+                    Console.WriteLine("Noi dung trong file: ");
+                    cs.ReadLog();
+                }
+                else if (String.Compare(choose, "Write", true) == 0)
+                {
+                    //Console.WriteLine("Nhap vao file:");
+                    //string rs = Console.ReadLine();
+                    for (int i = 0; i < 5; i++)
+                    {
+                        string temp = DateTime.Now.ToString();
+                        cs.WriteLogError(temp);
+                        Thread.Sleep(1000);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unknown");
+                }
+            }
+        }
+    }
+}
